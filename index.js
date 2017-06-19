6ff74d109e1876838e5068f16d94e44f6a21ac50
Jens Fischer <jens@weareprofound.com>
2017-06-19 17:55:55 -0400

    renaming to IconfontPlugin

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import globParent from 'glob-parent';
 import path from 'path';
 import webfont from 'webfont';
 
-export default class WebfontPlugin {
+export default class IconfontPlugin {
     constructor(options = {}) {
         if (!options.files) {
             throw new Error('Require `files` options');
@@ -36,73 +36,7 @@ export default class WebfontPlugin {
     compile(callback) {
         const { options } = this;
 
-        return nodify(
-            webfont(options).then(result => {
-                console.log(result);
-                const { fontName } = result.config;
-                const dest = path.resolve(this.options.dest.fontsDir);
-
-                let destStyles = null;
-
-                if (result.styles) {
-                    if (this.options.dest.stylesDir) {
-                        destStyles = path.resolve(this.options.dest.stylesDir);
-                    }
-
-                    if (!destStyles) {
-                        destStyles = dest;
-                    }
-
-                    if (result.usedBuildInStylesTemplate) {
-                        destStyles = path.join(
-                            destStyles,
-                            `${result.config.fontName}.${result.config
-                                .template}`
-                        );
-                    } else {
-                        destStyles = path.join(
-                            destStyles,
-                            path
-                                .basename(result.config.template)
-                                .replace('.njk', '')
-                        );
-                    }
-                }
-
-                return Promise.all(
-                    Object.keys(result).map(type => {
-                        if (
-                            type === 'config' ||
-                            type === 'usedBuildInStylesTemplate'
-                        ) {
-                            return Promise.resolve();
-                        }
-
-                        const content = result[type];
-                        let destFilename = null;
-
-                        if (type !== 'styles') {
-                            destFilename = path.resolve(
-                                path.join(dest, `${fontName}.${type}`)
-                            );
-                        } else {
-                            destFilename = path.resolve(destStyles);
-                        }
-
-                        return new Promise((resolve, reject) => {
-                            fs.outputFile(destFilename, content, error => {
-                                if (error) {
-                                    return reject(new Error(error));
-                                }
-
-                                return resolve();
-                            });
-                        });
-                    })
-                );
-            }),
-            error => callback(error)
-        );
+        return callback();
     }
 
     watch(compilation, callback) {
