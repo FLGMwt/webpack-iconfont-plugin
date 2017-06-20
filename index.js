b633944f299195244fa140f0bea23be9797adaa1
Jens Fischer <jens@weareprofound.com>
2017-06-20 13:09:08 -0400

    removed webfont dependency

--- index.js
+++ index.js
@@ -2,53 +2,104 @@ import nodify from 'nodeify';
 import fs from 'fs-extra';
 import globParent from 'glob-parent';
 import path from 'path';
-import webfont from 'webfont';
+import iconfont from './engine'
 
 export default class IconfontPlugin {
-    constructor(options = {}) {
-        if (!options.files) {
-            throw new Error('Require `files` options');
-        }
+  constructor(options = {}) {
+    const required = ['svgs', 'fonts', 'styles'];
 
-        if (!options.dest) {
-            throw new Error('Require `dest` options');
-        }
+    required.forEach(function(r) {
+      if (!options[r]) {
+        throw new Error(`Require '${r}' option`);
+      }
+    })
 
-        this.options = Object.assign({
-            template: './template.scss'
-        }, options);
-        console.log(this.options);
-        this.fileDependencies = [];
-    }
+    this.options = Object.assign({}, options);
+    this.fileDependencies = [];
 
-    apply(compiler) {
-        compiler.plugin('run', (compilation, callback) =>
-            this.compile(callback)
-        );
-        compiler.plugin('watch-run', (compilation, callback) =>
-            this.compile(callback)
-        );
-        compiler.plugin('after-emit', (compilation, callback) =>
-            this.watch(compilation, callback)
-        );
-    }
+    this.compile = this.compile.bind(this);
+    this.watch = this.watch.bind(this);
+  }
 
-    compile(callback) {
-        const { options } = this;
+  apply(compiler) {
+    compiler.plugin('run', this.compile);
+    compiler.plugin('watch-run', this.compile);
+    compiler.plugin('after-emit', this.watch);
+  }
 
-        return callback();
-    }
+  compile(compilation, callback) {
+    const { options } = this;
+    return nodify(
+      iconfont(options).then(result => {
+          const { fontName } = result.config;
+          let destStyles = null;
 
-    watch(compilation, callback) {
-        const globPatterns = typeof this.options.files === 'string' ? [this.options.files] : this.options.files;
+          if (result.styles) {
+              if (this.options.styles) {
+                  destStyles = path.resolve(this.options.styles);
+              }
 
-        globPatterns.forEach(globPattern => {
-            const context = globParent(globPattern);
-            if (compilation.contextDependencies.indexOf(context) === -1) {
-                compilation.contextDependencies.push(context);
-            }
-        });
+              if (result.usedBuildInStylesTemplate) {
+                  destStyles = path.join(
+                      destStyles,
+                      `${result.config.fontName}.${result.config.template}`
+                  );
+              } else {
+                  destStyles = path.join(
+                      destStyles,
+                      path
+                          .basename(result.config.template)
+                          .replace('.njk', '')
+                  );
+              }
+          }
 
-        return callback();
-    }
+          return Promise.all(
+              Object.keys(result).map(type => {
+                  if (
+                      type === 'config' ||
+                      type === 'usedBuildInStylesTemplate'
+                  ) {
+                      return Promise.resolve();
+                  }
+
+                  const content = result[type];
+                  let destFilename = null;
+
+                  if (type !== 'styles') {
+                      destFilename = path.resolve(
+                          path.join(this.options.fonts, `${fontName}.${type}`)
+                      );
+                  } else {
+                      destFilename = path.resolve(destStyles);
+                  }
+
+                  return new Promise((resolve, reject) => {
+                      fs.outputFile(destFilename, content, error => {
+                          if (error) {
+                              return reject(new Error(error));
+                          }
+
+                          return resolve();
+                      });
+                  });
+              })
+          );
+      }),
+      error => callback(error)
+    );
+  }
+
+  watch(compilation, callback) {
+    const globPatterns = typeof this.options.svgs === 'string' ? [this.options.svgs] : this.options.svgs;
+
+    globPatterns.forEach(globPattern => {
+      const context = globParent(globPattern);
+      if (compilation.contextDependencies.indexOf(context) === -1) {
+        compilation.contextDependencies.push(context);
+      }
+    });
+
+    return callback();
+  }
 }
