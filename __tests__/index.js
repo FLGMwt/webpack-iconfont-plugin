2f28856655f849ab58b0b9cc1a81e5ebd9736009
Jens Fischer <jens@weareprofound.com>
2017-06-22 17:30:02 -0400

    testing webpack config

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,11 +1,16 @@
 import path from 'path';
+import del from 'del';
 import IconfontPlugin from '../';
+import webpackConfigBase from './config/base';
+import webpack from 'webpack';
 
 const baseConfig = {
   svgs: path.resolve(__dirname, 'assets/svgs/**/*.svg'),
   fonts: path.resolve(__dirname, 'assets/fonts'),
   styles: path.resolve(__dirname, 'assets/scss')
 };
+
+const assets = path.resolve(__dirname, 'assets');
 
 describe('IconfontPlugin initialization', () => {
   it('should export `IconfontPlugin` as a class', () => {
@@ -47,5 +52,28 @@ describe('IconfontPlugin initialization', () => {
     expect(compiler.plugin).toHaveBeenCalledWith('after-emit', plugin.watch);
     expect(compiler.plugin).toHaveBeenCalledWith('watch-run', plugin.compile),
     expect(compiler.plugin).toHaveBeenCalledWith('run', plugin.compile);
+  });
+});
+
+beforeEach(() =>
+  del([
+    path.resolve(__dirname, 'build'),
+    `${assets}/fonts`,
+    `${assets}/styles/iconfont.css`
+  ])
+);
+
+describe('Webpack execution', () => {
+  it('should execute successfully', () => {
+    const options = Object.assign({}, baseConfig);
+
+    webpackConfigBase.plugins = [new IconfontPlugin(options)];
+
+    webpack(webpackConfigBase, (error, stats) => {
+        if (error) {
+            throw error;
+        }
+        expect(stats.compilation.errors.length).toEqual(0);
+    });
   });
 });
